a426214f69a3262cc8a1f30f8d02aa454b991efd
Agusioma <terryterrence200@gmail.com>
2024-07-02 16:33:32 +0300

    refactors

--- components/PagesScreen.js
+++ components/PagesScreen.js
@@ -3,32 +3,18 @@ import React from 'react';
 import { View, Text, FlatList, RefreshControl, ActivityIndicator } from 'react-native';
 import styles from '../styles/listingstyling'; // Adjust styling if necessary
 import fetchData from './utils/fetchData';
+import DataListScreen from "./utils/DataListScreen";
 
 const PagesScreen = () => {
     const { data, loading, refreshing, onRefresh } = fetchData('https://8877-41-80-116-93.ngrok-free.app/wordpress/wp-json/wp/v2/pages'); // Replace with your endpoint URL
 
-    const renderItem = ({ item }) => (
-        <View style={styles.postContainer}>
-            <Text style={styles.postTitle}>{item.title.rendered}</Text>
-            <Text style={styles.postDate}>Last modified: {item.modified} UTC</Text>
-        </View>
-    );
-
     return (
-        <View style={styles.container}>
-            {loading ? (
-                <ActivityIndicator size="large" color="#0000ff" />
-            ) : (
-                <FlatList
-                    data={data}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={renderItem}
-                    refreshControl={
-                        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-                    }
-                />
-            )}
-        </View>
+        <DataListScreen
+            loading={loading}
+            data={data}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+        />
     );
 };
 
--- components/PostsScreen.js
+++ components/PostsScreen.js
@@ -1,34 +1,17 @@
-import {StatusBar} from 'expo-status-bar';
-import React, {useCallback, useEffect, useState} from 'react';
-import {View, Text, FlatList, StyleSheet, RefreshControl, ActivityIndicator} from 'react-native';
-import styles from '../styles/listingstyling';
+import React from 'react';
 import fetchData from "./utils/fetchData";
+import DataListScreen from "./utils/DataListScreen";
 
 const PostsScreen = () => {
     const { data, loading, refreshing, onRefresh } = fetchData('https://8877-41-80-116-93.ngrok-free.app/wordpress/wp-json/wp/v2/posts'); // Replace with your endpoint URL
 
-    const renderItem = ({ item }) => (
-        <View style={styles.postContainer}>
-            <Text style={styles.postTitle}>{item.title.rendered}</Text>
-            <Text style={styles.postDate}>Last modified: {item.modified} UTC</Text>
-        </View>
-    );
-
     return (
-        <View style={styles.container}>
-            {loading ? (
-                <ActivityIndicator size="large" color="#0000ff" />
-            ) : (
-                <FlatList
-                    data={data}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={renderItem}
-                    refreshControl={
-                        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-                    }
-                />
-            )}
-        </View>
+        <DataListScreen
+            loading={loading}
+            data={data}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+        />
     );
 };
 
